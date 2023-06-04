7c74b363ff1235bc745421cbebc3b62e05443342
HeinrichGomTag <0241823@up.edu.mx>
2023-06-04 15:13:55 -0600

    Scatter Dist

---
+++ barchartDist.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+daa <- read.csv("resultados.csv")
+
+
+# Scatter plot of distance values
+plt_distancia <- ggplot(data, aes(x = salon, y = distancia)) +
+  geom_point() +
+  labs(x = "Salón", y = "Distancia", title = "Valores de Distancia")
+
+plt_distancia
+
