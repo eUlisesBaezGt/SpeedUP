562d2480b30092ea62c3c84ab374941c3c739915
kikinacademy <kikinacademy@gmail.com>
2023-06-03 21:45:46 -0600

    Nueva tabla 2 (R)
    Co-Authored-By: Heinrich <89673617+HeinrichGomTag@users.noreply.github.com>

--- freqtab.R
+++ freqtab.R
@@ -4,31 +4,30 @@ library(tidyr)
 # Leer los datos del archivo resultados.csv
 data <- read.csv("resultados.csv")
 
-velocidad_subida <- data$up
-velocidad_bajada <- data$down
-
 # Definir los intervalos de clase
-rango_min <- floor(min(velocidad_subida, na.rm = TRUE) / 10) * 10
-rango_max <- ceiling(max(velocidad_subida, na.rm = TRUE) / 10) * 10
+rango_min <- floor(min(c(data$up, data$down)) / 10) * 10
+rango_max <- ceiling(max(c(data$up, data$down)) / 10) * 10
 rangos <- seq(rango_min, rango_max, by = 10)
 
 # Tabla de frecuencia de velocidad de subida
-tabla_subida <- data.frame(Rango_de_Velocidad = cut(velocidad_subida, breaks = rangos, include.lowest = TRUE, na.include = TRUE)) %>%
-  count(Rango_de_Velocidad, .drop = FALSE) %>%
+tabla_subida <- data %>%
+  mutate(Rango_de_Velocidad = cut(up, breaks = rangos, include.lowest = TRUE)) %>%
+  count(Rango_de_Velocidad) %>%
   arrange(Rango_de_Velocidad) %>%
   rename(Frecuencia = n) %>%
-  mutate(Frecuencia_Relativa = Frecuencia / sum(Frecuencia, na.rm = TRUE),
+  mutate(Frecuencia_Relativa = Frecuencia / sum(Frecuencia),
          Frecuencia_Acumulada = cumsum(Frecuencia))
 
 # Convertir los intervalos de subida a formato de cadena
 tabla_subida$Rango_de_Velocidad <- as.character(tabla_subida$Rango_de_Velocidad)
 
 # Tabla de frecuencia de velocidad de bajada
-tabla_bajada <- data.frame(Rango_de_Velocidad = cut(velocidad_bajada, breaks = rangos, include.lowest = TRUE, na.include = TRUE)) %>%
-  count(Rango_de_Velocidad, .drop = FALSE) %>%
+tabla_bajada <- data %>%
+  mutate(Rango_de_Velocidad = cut(down, breaks = rangos, include.lowest = TRUE)) %>%
+  count(Rango_de_Velocidad) %>%
   arrange(Rango_de_Velocidad) %>%
   rename(Frecuencia = n) %>%
-  mutate(Frecuencia_Relativa = Frecuencia / sum(Frecuencia, na.rm = TRUE),
+  mutate(Frecuencia_Relativa = Frecuencia / sum(Frecuencia),
          Frecuencia_Acumulada = cumsum(Frecuencia))
 
 # Convertir los intervalos de bajada a formato de cadena
